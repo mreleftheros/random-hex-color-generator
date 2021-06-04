e8a6618515cc24fcd5b0b551984185351b882b8f
eleftheros <eleftheros_all@protonmail.com>
2021-06-04 07:25:26 +0300

    Add generateHex fn

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,26 @@
 const generateBtn = document.getElementById("generateBtn");
 const restoreBtn = document.getElementById("restoreBtn");
 const colorValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+let currentHex;
 
 // utility function to generate random index
 const getRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// const generateHexColor = () => {
-//   let len = colorValues.length;
-//   let hexColor = "";
-//   for(let i = 0; i < 6; i++) {
+// function to generate hex color
+const generateHex = () => {
+  currentHex = "#";
+  let len = colorValues.length;
 
-//   }
-// };
+  for(let i = 0; i < 6; i++) {
+    let randomIndex = getRandom(0, len - 1);
+    let value = colorValues[randomIndex];
 
-// events
-// window.addEventListener("DOMContentLoaded", generateHexColor);
+    currentHex += value;
+  }
+  console.log(currentHex)
+};
+
+// event listeners
+window.addEventListener("DOMContentLoaded", generateHex);
